e133752c9604f14f109e1f9518b7bef3a858bca0
GABRIEL KAICON BATISTA HILARIO <0048609@IFMG.EDU.BR>
2022-08-03 08:14:15 -0300

    Criação de pasta na aula 1 de AEDs I

---
+++ Pratica_AEDs_I/ex_PraticaI/ex_PraticoI.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+ 
+using namespace std;
+ 
+int main() {
+  UINT CPAGE_UTF8 = 65001;
+  UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+  SetConsoleOutputCP(CPAGE_UTF8);
+ 
+  //atv 1
+ 
+ 
+  cout << endl;
+  return 0;
+}
