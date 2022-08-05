9b0108916033edf56bdb665e1503a6f8fe636173
GABRIEL KAICON BATISTA HILARIO <0048609@IFMG.EDU.BR>
2022-08-05 09:36:02 -0300

    Alteração de teste

--- Pratica_AEDs_I/ex_PraticaII/ex_PraticoII.cpp
+++ Pratica_AEDs_I/ex_PraticaII/ex_PraticoII.cpp
@@ -42,7 +42,7 @@ int main() {
     //média de número de mulheres com salário abaixo de 1000
     for(int i = 0; i < 20; i++){
         if(h[i].sexo == 'F' || h[i].sexo == 'f' && h[i].salario <= 1000){
-
+            //comente
         }
     }
     return 0;
