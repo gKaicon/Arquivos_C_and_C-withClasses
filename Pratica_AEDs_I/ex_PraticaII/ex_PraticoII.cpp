c309ec0d52b19b0996b541c88d6d606c36d682af
GABRIEL KAICON BATISTA HILARIO <0048609@IFMG.EDU.BR>
2022-08-08 19:57:37 -0300

    Alteração em Monitoria

--- Pratica_AEDs_I/ex_PraticaII/ex_PraticoII.cpp
+++ Pratica_AEDs_I/ex_PraticaII/ex_PraticoII.cpp
@@ -15,9 +15,10 @@ int main() {
     UINT CPAGE_UTF8 = 65001;
     UINT CPAGE_DEFAULT = GetConsoleOutputCP();
     SetConsoleOutputCP(CPAGE_UTF8);
-    double somaSa, somaFi, mediaSa, mediaFi, maiorSa, percMulher;
+    double somaSa, somaFi, mediaSa, mediaFi, maiorSa, percMulher, somaSM, qtdMulher, qtdH;
     Habitante h[20];
-    for(int i = 0; i < 20; i++){
+    qtdH = 5;
+    for(int i = 0; i < qtdH; i++){
         cout << "Informe o salário: R$ ";
         cin >> h[i].salario;
         cout << "Informe a idade: ";
@@ -28,23 +29,29 @@ int main() {
         cin >> h[i].sexo;
     }
     //média de salário da população
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < qtdH; i++){
         somaSa = somaSa + h[i].salario;
     }
-    mediaSa = somaSa/20;
+    mediaSa = somaSa/qtdH;
 
     //média de número de filhos da população
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < qtdH; i++){
         somaFi = somaFi + h[i].numFilho;
     }
-    mediaSa = somaFi/20;
+    mediaSa = somaFi/qtdH;
     
     //percentual de número de mulheres com salário abaixo de 1000
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < qtdH; i++){
         if(h[i].sexo == 'F' || h[i].sexo == 'f' && h[i].salario <= 1000){
-            //comente
+            qtdMulher++;
         }
     }
     //maior salário
+    for(int i = 0; i < qtdH; i++){
+        if(h[i].salario > maiorSa){
+            maiorSa = h[i].salario;
+        }
+    }
     return 0;
 }
+
