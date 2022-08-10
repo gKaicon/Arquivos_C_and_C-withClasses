10d65f7badca29672bcda41bf10fe5dc908dcf97
GABRIEL KAICON BATISTA HILARIO <0048609@IFMG.EDU.BR>
2022-08-10 15:50:09 -0300

    Alteração no Lab2

--- Uso de arquivos - AEDs/Exercícios/ExercícioPrático_II/Ex_Pratico.cpp
+++ Uso de arquivos - AEDs/Exercícios/ExercícioPrático_II/Ex_Pratico.cpp
@@ -4,11 +4,34 @@
 
 using namespace std;
 
+void escreve_arquivo(){
+    ofstream file;
+
+    file.open("tabela.txt");
+
+    if (!file.is_open()){
+        cerr << "ERRO! Não foi possível abrir o arquivo .txt \n";
+    }
+    file << "result";
+}
+void ler_arquivo(){
+    ifstream file;
+    string result;
+    file.open("tabela.txt");
+    if (!file.is_open()){
+        cerr << "ERRO! Não foi possível abrir o arquivo .txt \n";
+    }
+    file >> result;
+    while (!file.eof()){
+//        getline(file, result);
+        cout << result << endl;
+    }
+}   
+
 int main() {
     UINT CPAGE_UTF8 = 65001;
     UINT CPAGE_DEFAULT = GetConsoleOutputCP();
     SetConsoleOutputCP(CPAGE_UTF8);
-
     cout << sizeof(char) << "Byte(s)";
     cout << sizeof(int) << "Byte(s)";
     cout << endl;
